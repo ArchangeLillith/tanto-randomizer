52a32682ae48bf8614302c57608348ce79908e1d
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-08-21 16:24:01 -0400

    Deploy attem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MainPage from "./views/MainPage";
 //For testing purposes we can toggle off the styling to make it plain mardown
-import "./app.css";
 
 const App: React.FC = () => {
 	return <MainPage />;
