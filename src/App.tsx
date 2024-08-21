535dfb0cf28786dc52fbf38d982910f65a8e1fe4
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-08-21 16:31:53 -0400

    Attempting to add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainPage from "./views/MainPage";
+import "./App.css";
 //For testing purposes we can toggle off the styling to make it plain mardown
 
 const App: React.FC = () => {
